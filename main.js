ad67cd4cad583aa7ba8cd509bd8899b56646744c
Md Rakibul Islam <r4k1b.m4r5@gmail.com>
2023-12-14 03:22:52 +0600

    Add cube and render the scene

--- main.js
+++ main.js
@@ -12,3 +12,13 @@ camera.position.z = 2;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+renderer.render(scene, camera);
